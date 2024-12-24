60f3f2563d50865751674bd64178af79dfd17e46
nourhene amdouni <nourhene803@gmail.com>
2024-12-24 09:04:05 +0100

    page home login sing up html css js

--- my-angular-app/src/app/sign-up/sign-up.component.ts
+++ my-angular-app/src/app/sign-up/sign-up.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-sign-up',
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './sign-up.component.html',
   styleUrl: './sign-up.component.css'
 })
